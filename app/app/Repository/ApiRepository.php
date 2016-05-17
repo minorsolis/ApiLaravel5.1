d442974dcaadf697987f51300025c96f355ca509
Minor Solis <minorsolis@gmail.com>
2016-05-17 17:49:48 -0600

    Adding configuration to create backups on ec2

--- app/app/Repository/ApiRepository.php
+++ app/app/Repository/ApiRepository.php
@@ -2,6 +2,7 @@
 
 namespace app\Repository;
 use AWS;
+use Config;
 
 class ApiRepository{
 
@@ -12,8 +13,57 @@ class ApiRepository{
 	 */
 	public static function api($params){
 
-		$ec2 			= AWS::createClient('ec2');
-		dd($ec2);
+		$function 				= @$params['function'];
+
+		if($function == 'deregisterImage'){
+			return ApiRepository::deregisterImage($params);
+		
+		}elseif($function == 'createImage'){
+			return ApiRepository::createImage($params);
+
+		}
+
+	}
+
+	/**
+	 * [createImage description]
+	 * @param  [type] $params [description]
+	 * @return [type]         [description]
+	 */
+	public static function createImage($params){
+
+	}
+
+
+	/**
+	 * [cleanImages description]
+	 * @param  [type] $params [description]
+	 * @return [type]         [description]
+	 */
+	public static function deregisterImage($params){
+		$function 						= @$params['function'];
+		$total 							= 0;
+		$ec2 							= AWS::createClient('ec2');
+		$imageRaw 						= $ec2->describeImages(array('Owners'=> array('self') ));
+		$awsImageSufix 					= Config::get('api.awsImageSufix');
+
+		$imageArray 					= $imageRaw['Images'];
+
+		if(is_array($imageArray)){
+			foreach($imageArray AS $row){
+				$imageId 				= @$row['ImageId'];
+
+				$date_from 				= @str_ireplace($awsImageSufix, '', $row['Name'] );
+				$date_from 				= date("Ymd", strtotime($date_from));
+
+				if($date_from >= Config::get('api.awsImageDateFrom') ){
+					$delete 			= $ec2->deregisterImage( array( 'ImageId' => $imageId ) );
+					print_r($delete)."<br />";
+					++$total;
+				}
+			}
+		}
+		echo "<h3>Total $function = $total</h3>";
 	}
 
 }
---
+++ app/config/api.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+
+    'awsImageSufix' 			=> array('-prod','-dev','-proxy'),
+    'awsImageDateFrom'   		=> '20160101', 
+    'awsImageDay'   			=> 7, 
+];
